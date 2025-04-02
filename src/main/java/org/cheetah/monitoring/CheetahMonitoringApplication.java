f94685e5a507fd13abfdbe95e711a0245950acbc
Pelli, Edoardo Maria <edoardo.pelli@stefanini.com>
2025-04-02 17:01:17 +0200

    first commit

---
+++ src/main/java/org/cheetah/monitoring/CheetahMonitoringApplication.java
@@ -0,0 +1,13 @@
+package org.cheetah.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheetahMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheetahMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cheetah/monitoring/CheetahMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cheetah.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheetahMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
